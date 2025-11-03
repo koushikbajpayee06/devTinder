d764db63eac5bcd7bfcde5a8cee53bef0d5abcb9
Koushik <Koushikbajpayee06@gmail.com>
2025-11-03 16:22:59 +0530

    add query parameter handling with req.query

--- src/app.js
+++ src/app.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const app = express();
-// This will only handle GET call to /user
 
-app.use("/user",(req,res)=>{
-    res.send("HAHAHHAHHAHA");
-})
+// req /user , /user/xyz, /user/1
 app.get("/user",(req,res)=>{
+    console.log(req.query)
     res.send({firstName:"Koushik", lastName:"Bajpayee"})
 });
 
-app.post("/user",(req,res)=>{
-    //saving data to db
-    res.send("data Successfully saved to database!");
-});
-app.delete("/user",(req,res)=>{
-    //saving data to db
-    res.send("Deleated successfully");
-});
-
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
 
 app.listen(3000, () => {
   console.log("Server successfully running on Port 3000");
