db4df516ca9cb26a9499e979700ecf3053a8d038
Koushik <Koushikbajpayee06@gmail.com>
2025-11-03 13:17:43 +0530

    using nodemon to run express

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-
 const express = require("express");
-
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from dashboard");
-});
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
-});
-app.use("/hello",(req,res)=>{
-    res.send("Hello! Hello ! Hello");
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
 });
 
-app.listen(3000,()=>{
-    console.log("Server successfully running on Port 3000");
+app.use("/hello", (req, res) => {
+  res.send("Hello! Hello ! Hello Baju");
+});
+
+app.use("/", (req, res) => {
+  res.send("Namaste Koushik Bajpayee ji");
+});
+
+app.listen(3000, () => {
+  console.log("Server successfully running on Port 3000");
 });
