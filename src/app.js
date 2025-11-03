5fc3f086d2b1030092760a8e97f860343319a72f
Koushik <Koushikbajpayee06@gmail.com>
2025-11-03 13:02:17 +0530

    Refactor single catch-all route into separate routes for '/', '/test', and '/hello'

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res)=>{
+app.use("/",(req,res)=>{
+    res.send("Hello from dashboard");
+});
+app.use("/test",(req,res)=>{
     res.send("Hello from the server");
+});
+app.use("/hello",(req,res)=>{
+    res.send("Hello! Hello ! Hello");
 });
 
 app.listen(3000,()=>{
