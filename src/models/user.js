cfab06f88aa50bf247e165cf20b41475c02aca83
Koushik <Koushikbajpayee06@gmail.com>
2025-11-05 13:53:08 +0530

    created a model

--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,5 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model("User",userSchema);
-module.exports = User;
+module.exports = mongoose.model("User",userSchema);
 
