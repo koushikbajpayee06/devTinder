d06ec4bad2355fea441f1c613b2451ce1d83cbb7
Koushik <Koushikbajpayee06@gmail.com>
2025-11-12 13:21:40 +0530

    return user details on successful login instead of message  add CORS dependency for cross-origin request handling,configure CORS middleware to allow requests from localhost:5173

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const connectDB = require('./config/database');
+const cors = require('cors')
 const app = express();
 const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}));
 
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
